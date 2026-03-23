299c33a82ba6ca8654ba3a02be9332b314a5df9d
mickgv-web <mickgv@gmail.com>
2026-03-23 18:01:32 +0100

    Add Vercel SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./components/layout/Layout";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 // Importarías una sección de contacto si la tienes aparte
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       </section>   */}
 
       <ScrollToTop />
+      <SpeedInsights />
     </Layout>
   );
 }
